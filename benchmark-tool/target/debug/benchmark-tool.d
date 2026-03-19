d82b7ea764771d6859b11488f0ad553a4444a1a7
meteormin <miniyu97@gmail.com>
2026-03-19 19:25:23 +0900

    Apply .gitignore and remove ignored files

--- benchmark-tool/target/debug/benchmark-tool.d
+++
@@ -1,1 +0,0 @@
-/Users/yooseongmin/gemini-chat/todo-api-bench/benchmark-tool/target/debug/benchmark-tool: /Users/yooseongmin/gemini-chat/todo-api-bench/benchmark-tool/src/main.rs
--- benchmark-tool/target/debug/deps/benchmark_tool-83a9ac2cb5b87748.d
+++
@@ -1,5 +0,0 @@
-/Users/yooseongmin/gemini-chat/todo-api-bench/benchmark-tool/target/debug/deps/benchmark_tool-83a9ac2cb5b87748.d: src/main.rs
-
-/Users/yooseongmin/gemini-chat/todo-api-bench/benchmark-tool/target/debug/deps/benchmark_tool-83a9ac2cb5b87748: src/main.rs
-
-src/main.rs:
